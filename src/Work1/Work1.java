7058890789121c0746e9eb2befc2166d07697778
nastromass@gmail.com <nastromass@gmail.com>
2022-08-23 22:46:30 +0300

    Задача №2

--- src/Work1/Work1.java
+++ src/Work1/Work1.java
@@ -1,6 +1,10 @@
 package Work1;
 
+import java.util.Scanner;
+
 public class Work1 {
+
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         //Задача №1
         String hi = "                Hello ";
@@ -12,5 +16,16 @@ public class Work1 {
         String nWord = nHi + lWord + newLine;
 
         System.out.print(nWord.repeat(3));
+
+        //Задача №2
+
+        System.out.println("\nВведите рост в метрах. Через запятую");
+        float growth = scanner.nextFloat();
+
+        System.out.println("Введите вес");
+        float weight = scanner.nextFloat();
+
+        float bodyMassIndex = weight/(growth * growth);
+        System.out.println("Индекса массы тела " + bodyMassIndex);
     }
 }
