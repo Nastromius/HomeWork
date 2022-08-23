526393e7f1f6f7573aa57877390510e85b268dbe
nastromass@gmail.com <nastromass@gmail.com>
2022-08-23 22:32:57 +0300

    Задача №1

--- src/Work1/Work1.java
+++ src/Work1/Work1.java
@@ -1,4 +1,16 @@
 package Work1;
 
 public class Work1 {
+    public static void main(String[] args) {
+        //Задача №1
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+
+        String nHi = hi.trim();
+        String lWord = world.toLowerCase();
+        String nWord = nHi + lWord + newLine;
+
+        System.out.print(nWord.repeat(3));
+    }
 }
