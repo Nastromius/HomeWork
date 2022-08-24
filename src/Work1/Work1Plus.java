4ed2dce3e2f174b9e221102c254f94ee277d6f74
nastromass@gmail.com <nastromass@gmail.com>
2022-08-24 22:04:59 +0300

    Задача №1

--- src/Work1/Work1Plus.java
+++ src/Work1/Work1Plus.java
@@ -2,6 +2,15 @@ package Work1;
 
 public class Work1Plus {
     public static void main(String[] args) {
+        //№1
+        {
+            String strNumber = "234";
+            String text = "some_text";
 
+            int numb = Integer.parseInt(strNumber);
+            int nText = text.length();
+
+            System.out.println("Результат сложения: " + (numb + nText));
+        }
     }
 }
