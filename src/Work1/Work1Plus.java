5f9dfa6e8c92c3426fb99515963350e73486b398
nastromass@gmail.com <nastromass@gmail.com>
2022-08-24 22:18:58 +0300

    Задача №2+

--- src/Work1/Work1Plus.java
+++ src/Work1/Work1Plus.java
@@ -12,5 +12,14 @@ public class Work1Plus {
 
             System.out.println("Результат сложения: " + (numb + nText));
         }
+        //2
+        {
+            int a = 3;
+            int b = 5;
+
+            int c = (a + b)^2; // ^ - знак не понятен
+
+            System.out.println("Результат: " + c);
+        }
     }
 }
